f7f0df743fb58b6ccce236fa2c5c2d7ab81434da
Tim Perkins <tprk77@gmail.com>
2018-07-21 00:49:17 -0400

    Added builder role

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // Copyright (c) 2018 Tim Perkins
 
+import { Builder } from "./roles/builder";
 import { Harvester } from "./roles/harvester";
 
 declare global {
@@ -19,6 +20,7 @@ export const loop = () => {
   }
 
   const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === "harvester");
+  const builders = _.filter(Game.creeps, (creep) => creep.memory.role === "builder");
 
   if (harvesters.length < 4) {
     const newName = "Harvester" + Game.time;
@@ -26,12 +28,20 @@ export const loop = () => {
     Game.spawns.Spawn1.spawnCreep(
       [CARRY, CARRY, WORK, WORK, MOVE, MOVE],
       newName, {memory: {role: "harvester"} as CreepMemory});
+  } else if (builders.length < 4) {
+    const newName = "Builder" + Game.time;
+    console.log("Spawning new builder: " + newName);
+    Game.spawns.Spawn1.spawnCreep(
+      [CARRY, CARRY, WORK, WORK, MOVE, MOVE],
+      newName, {memory: {role: "builder"} as CreepMemory});
   }
 
   for (const name in Game.creeps) {
     const creep = Game.creeps[name];
     if (creep.memory.role === "harvester") {
       Harvester.run(creep);
+    } else if (creep.memory.role === "builder") {
+      Builder.run(creep);
     }
   }
 };
