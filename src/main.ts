440e22f0b4b5f29f067d38b9ca96dde2b2d160b7
Tim Perkins <tprk77@gmail.com>
2018-07-21 01:21:12 -0400

    Added basic game loop

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2018 Tim Perkins
+
+export const loop = () => {
+  // Automatically delete memory of missing creeps
+  for (const name in Memory.creeps) {
+    if (!(name in Game.creeps)) {
+      delete Memory.creeps[name];
+    }
+  }
+};
