ea713e1482040fd4c55f8af769357a8d4dc527be
Tim Perkins <tprk77@gmail.com>
2018-07-29 00:45:15 -0400

    Better state management for harvester

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -1,13 +1,21 @@
 // Copyright (c) 2018 Tim Perkins
 
+declare global {
+  interface CreepMemory {
+    harvesting: boolean;
+  }
+}
+
 export class Harvester {
   public static run(creep: Creep): void {
-    if (creep.carry.energy < creep.carryCapacity) {
-      const sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[1], {visualizePathStyle: {stroke: "#ffaa00"}});
-      }
-    } else {
+    if (!creep.memory.harvesting && creep.carry.energy === 0) {
+      creep.memory.harvesting = true;
+      creep.say("Harvest");
+    } else if (creep.memory.upgrading && creep.carry.energy === creep.carryCapacity) {
+      creep.memory.harvesting = false;
+      creep.say("Deposit");
+    }
+    if (!creep.memory.harvesting) {
       const consumers = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) =>
           ((structure.structureType === STRUCTURE_EXTENSION
@@ -19,6 +27,11 @@ export class Harvester {
         if (creep.transfer(consumers[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
           creep.moveTo(consumers[0], {visualizePathStyle: {stroke: "#ffffff"}});
         }
+      }
+    } else {
+      const sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[1], {visualizePathStyle: {stroke: "#ffaa00"}});
       }
     }
   }
