56dc5f08852332143e5bf5706cbb62a6c5871c08
Tim Perkins <tprk77@gmail.com>
2018-08-11 23:33:35 -0400

    Better source selection
    But it's still pretty bad.

--- src/creeps/tasks/harvest.ts
+++ src/creeps/tasks/harvest.ts
@@ -2,14 +2,27 @@
 
 export class Harvest {
   public static run(creep: Creep): boolean {
-    const sources = creep.room.find(FIND_SOURCES);
-    if (sources.length) {
-      if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[1], {visualizePathStyle: {stroke: "#ffaa00"}});
+    const source = Harvest.pickSource(creep, creep.room.find(FIND_SOURCES));
+    if (source) {
+      if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(source, {visualizePathStyle: {stroke: "#ffaa00"}});
       }
       return true;
     } else {
       return false;
+    }
+  }
+
+  /**
+   * This just switches in between the #0 and #1 source based on the creep's ID. It's obviously not
+   * that great at doing a good distribution, and doesn't consider source proximity.
+   */
+  private static pickSource(creep: Creep, sources: Source[]): Source|null {
+    if (sources.length) {
+      const sourceIndex = _.last(creep.id).charCodeAt(0) % sources.length;
+      return sources[sourceIndex];
+    } else {
+      return null;
     }
   }
 }
