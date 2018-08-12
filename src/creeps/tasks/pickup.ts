dfd33752b6dc75e9118fa789c3a2bd5dd286c1d5
Tim Perkins <tprk77@gmail.com>
2018-08-11 23:33:35 -0400

    Added pickup task

---
+++ src/creeps/tasks/pickup.ts
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 Tim Perkins
+
+export class Pickup {
+  public static run(creep: Creep): boolean {
+    const drops = creep.room.find(FIND_DROPPED_RESOURCES, {
+      filter: (drop) => creep.pos.inRangeTo(drop, 5),
+    });
+    if (drops.length) {
+      const drop = drops[0];
+      if (creep.pickup(drop) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(drop, {visualizePathStyle: {stroke: "#ffaa00"}});
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
