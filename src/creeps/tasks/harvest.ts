6af62a5c2220c4f2eae4af4f7af9f67439a89561
Tim Perkins <tprk77@gmail.com>
2018-08-12 19:50:24 -0400

    Allow source index for harvesters

--- src/creeps/tasks/harvest.ts
+++ src/creeps/tasks/harvest.ts
@@ -2,7 +2,9 @@
 
 export class Harvest {
   public static run(creep: Creep): boolean {
-    const source = Harvest.pickSource(creep, creep.room.find(FIND_SOURCES));
+    const sources = creep.room.find(FIND_SOURCES);
+    const source = (creep.memory.sourceIndex != null) ? sources[creep.memory.sourceIndex]
+                                                      : Harvest.pickSource(creep, sources);
     if (source) {
       if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
         creep.moveTo(source, {visualizePathStyle: {stroke: "#ffaa00"}});
