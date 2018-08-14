d6db07dd79d074258c1387a4902b9029b97ddba3
Tim Perkins <tprk77@gmail.com>
2018-08-13 23:20:02 -0400

    Tweaked pickup so it can exit early

--- src/creeps/tasks/pickup.ts
+++ src/creeps/tasks/pickup.ts
@@ -2,6 +2,9 @@
 
 export class Pickup {
   public static run(creep: Creep): boolean {
+    if (_.sum(creep.carry) === creep.carryCapacity) {
+      return false;
+    }
     const drops = creep.room.find(FIND_DROPPED_RESOURCES, {
       filter: (drop) => creep.pos.inRangeTo(drop, 5),
     });
