2a6748a921be653e85f38b39ff348c08b3c62cd9
Tim Perkins <tprk77@gmail.com>
2018-08-14 22:52:59 -0400

    Protect against null tasks

--- src/creeps/utils.ts
+++ src/creeps/utils.ts
@@ -4,6 +4,6 @@ interface Task {
   run: (creep: Creep) => boolean;
 }
 
-export function runTasks(creep: Creep, tasks: Task[]) {
-  _.some(tasks, (task) => task.run(creep));
+export function runTasks(creep: Creep, tasks: (Task|null)[]) {
+  _.some(tasks, (task) => task && task.run(creep));
 }
