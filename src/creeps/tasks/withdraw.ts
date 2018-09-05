8fb51fc34f62a18c78efdc88e0af2c39d1c0b189
Tim Perkins <tprk77@gmail.com>
2018-09-04 20:30:54 -0400

    Withdraw from nearest container

--- src/creeps/tasks/withdraw.ts
+++ src/creeps/tasks/withdraw.ts
@@ -2,12 +2,11 @@
 
 export class Withdraw {
   public static run(creep: Creep): boolean {
-    const containers = creep.room.find(FIND_STRUCTURES, {
+    const container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
       filter: (structure) =>
           structure.structureType === STRUCTURE_CONTAINER && !!structure.store.energy,
     });
-    if (containers.length) {
-      const container = containers[0];
+    if (container) {
       if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
         creep.moveTo(container, {visualizePathStyle: {stroke: "#ffaa00"}});
       }
