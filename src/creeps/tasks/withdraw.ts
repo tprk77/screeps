a1c6efa8fac694942d0a50749026b7ea17a211c1
Tim Perkins <tprk77@gmail.com>
2018-09-04 20:30:54 -0400

    Energy theshold for far containers

--- src/creeps/tasks/withdraw.ts
+++ src/creeps/tasks/withdraw.ts
@@ -1,10 +1,15 @@
 // Copyright (c) 2018 Tim Perkins
 
 export class Withdraw {
+  public static FAR_DISTANCE_THRESHOLD = 5;
+  public static FAR_ENERGY_THRESHOLD = 500;
+
   public static run(creep: Creep): boolean {
     const container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-      filter: (structure) =>
-          structure.structureType === STRUCTURE_CONTAINER && !!structure.store.energy,
+      filter: (structure) => (structure.structureType === STRUCTURE_CONTAINER
+                              && (creep.pos.inRangeTo(structure, this.FAR_DISTANCE_THRESHOLD)
+                                      ? !!structure.store.energy
+                                      : structure.store.energy > this.FAR_ENERGY_THRESHOLD)),
     });
     if (container) {
       if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
