6a254f641ee5e67b1c595bfee1f67e7f374504bb
Tim Perkins <tprk77@gmail.com>
2018-10-27 01:20:11 -0400

    Fill empty towers before extensions

--- src/creeps/tasks/deposit.ts
+++ src/creeps/tasks/deposit.ts
@@ -5,7 +5,12 @@ export class Deposit {
     if (!_.get(creep.room.controller, "my", false)) {
       return false;
     }
-    const spawnOrExtension = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+    // TODO This needs some hysteresis, it's going to trash around 25% energy
+    const emptyTower = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: (structure) => ((structure.structureType === STRUCTURE_TOWER)
+                              && structure.energy < 0.25 * structure.energyCapacity),
+    });
+    const spawnOrExtension = emptyTower ? null : creep.pos.findClosestByRange(FIND_STRUCTURES, {
       filter: (structure) => ((structure.structureType === STRUCTURE_SPAWN
                                || structure.structureType === STRUCTURE_EXTENSION)
                               && structure.energy < structure.energyCapacity),
@@ -14,7 +19,7 @@ export class Deposit {
       filter: (structure) => ((structure.structureType === STRUCTURE_TOWER)
                               && structure.energy < 0.75 * structure.energyCapacity),
     });
-    const consumer = spawnOrExtension || tower;
+    const consumer = emptyTower || spawnOrExtension || tower;
     if (consumer) {
       if (creep.transfer(consumer, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
         creep.moveTo(consumer, {visualizePathStyle: {stroke: "#ffffff"}});
